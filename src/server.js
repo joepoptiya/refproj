3755f978ed42100d1e8e87d9b6018a4e0370085a
Ferdeen <joe.poptiya@gmail.com>
2022-03-27 16:24:40 -0700

    Update

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5040;
+const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.get("/api/v1/todos", (req, res) => {
