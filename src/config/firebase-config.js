25d0b07faa2487d3d8c5044c1437214b4f2017cf
Ferdeen <joe.poptiya@gmail.com>
2022-03-27 17:28:22 -0700

    Added middleware with service token verification

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY_FILE);
+const serviceAccount = require("./service-account-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-modules.exports = admin;
+module.exports = admin;
--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const middleware = require("./middleware");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
+app.use(middleware.decodeToken);
 
 app.get("/api/v1/todos", (req, res) => {
   console.log(req.headers);
